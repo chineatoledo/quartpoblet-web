b9de0cd4b50635e21e51a2001c39615af47f901f
Alejandro Chinea <chineatoledo@hotmail.com>
2023-04-25 22:34:55 +0200

    Complete v1

---
+++ dist/index.816e7b21.js
@@ -0,0 +1,13 @@
+const carousel = document.querySelector(".carousel");
+const imgs = carousel.querySelectorAll("img");
+const intervalTime = 3000;
+let activeImg = 0;
+function changeImg() {
+    imgs[activeImg].classList.remove("active");
+    activeImg++;
+    if (activeImg === imgs.length) activeImg = 0;
+    imgs[activeImg].classList.add("active");
+}
+setInterval(changeImg, intervalTime);
+
+//# sourceMappingURL=index.816e7b21.js.map
